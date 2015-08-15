a0f81c2cfea81c0b5fe6df7d4ca7d52db7c156b9
alfinoc <chrisalfino@gmail.com>
2015-08-14 21:52:09 -0700

    added finer points to dad's schedule

--- routes.js
+++ routes.js
@@ -11,43 +11,60 @@ Routes.location = {
    Rome: { 'lat': 41.899723, 'lng': 12.487418 },
    Vienna: { 'lat': 48.200609, 'lng': 16.367068 },
    Prague: { 'lat': 50.069146, 'lng': 14.426485 },
-   Munich: { 'lat': 48.143687, 'lng': 11.576168 }
+   Munich: { 'lat': 48.143687, 'lng': 11.576168 },
+   Palermo: { 'lat': 38.122679, 'lng': 13.363468 },
+   Imst: { 'lat': 47.239627, 'lng': 10.741537 },
+   Nice: { 'lat': 43.705486, 'lng': 7.264705 },
+   Lucerne: { 'lat': 47.054872, 'lng': 8.309943 },
+   Giuncarico: { 'lat': 42.906487, 'lng': 10.991973 },
+   Marisco: { 'lat': 38.047309, 'lng': 13.119592 },
+   Naples: { 'lat': 40.848337, 'lng': 14.266831 }
 };
 
 Routes.chris = [
    {
       name: 'London',
-      arrive: '7/15',
-      depart: '7/21',
+      arrive: '7/16',
+      depart: '7/18',
    },
    {
       name: 'Berlin',
-      arrive: '7/22',
-      depart: '7/24',
+      arrive: '7/18',
+      depart: '7/20',
    },
    {
       name: 'Prague',
-      arrive: '7/24',
-      depart: '7/26',
+      arrive: '7/20',
+      depart: '7/23',
    },
    {
       name: 'Munich',
+      arrive: '7/23',
+      depart: '7/26',
+   },
+   {
+      name: 'Imst',
       arrive: '7/26',
-      depart: '7/29',
+      depart: '8/27',
+   },
+   {
+      name: 'Lucerne',
+      arrive: '7/27',
+      depart: '7/28',
    },
    {
       name: 'Zurich',
-      arrive: '7/29',
+      arrive: '7/28',
       depart: '8/1',
    },
    {
-      name: 'Marseille',
+      name: 'Nice',
       arrive: '8/1',
-      depart: '8/8',
+      depart: '8/6',
    },
    {
       name: 'Paris',
-      arrive: '8/1',
+      arrive: '8/6',
       depart: '8/8',
    },
    {
@@ -79,7 +96,32 @@ Routes.dad = [
    {
       name: 'Rome',
       arrive: '7/10',
-      depart: '8/1',
+      depart: '7/11',
+   },
+   {
+      name: 'Naples',
+      arrive: '7/11',
+      depart: '7/13',
+   },
+   {
+      name: 'Palermo',
+      arrive: '7/13',
+      depart: '7/16'
+   },
+   {
+      name: 'Marisco',
+      arrive: '7/16',
+      depart: '7/30'
+   },
+   {
+      name: 'Giuncarico',
+      arrive: '7/30',
+      depart: '8/3'
+   },
+   {
+      name: 'Paris',
+      arrive: '8/3',
+      depart: '8/8'
    },
    {
       name: 'Edinburgh',
@@ -92,7 +134,7 @@ Routes.nick = [
    {
       name: 'Vienna',
       arrive: '7/18',
-      depart: '8/1',
+      depart: '8/8',
    },
    {
       name: 'Edinburgh',
